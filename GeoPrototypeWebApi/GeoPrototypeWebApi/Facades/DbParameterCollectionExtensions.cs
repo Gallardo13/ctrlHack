ea3bb77fc1b02dbbd8bb92347db79bbc6301284c
Nikolay Chapkin <nic-ch-work@yandex.ru>
2018-09-22 17:45:25 +0300

    Метод расширитель для задания параметров в DbParameterCollection

---
+++ GeoPrototypeWebApi/GeoPrototypeWebApi/Facades/DbParameterCollectionExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Data.Common;
+
+namespace GeoPrototypeWebApi.Facades
+{
+    public static class DbParameterCollectionExtensions
+    {
+        public static void Add(this DbParameterCollection collection, string name, object value) =>
+            collection[collection.Add(value)].ParameterName = name;
+    }
+}
