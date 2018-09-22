a4ef069cfa742fec451ddb0e69e61c1c29cd00e6
Nikolay Chapkin <nic-ch-work@yandex.ru>
2018-09-22 19:34:55 +0300

    Настройка паблиша + доделал GetNearObjectsContoller

---
+++ GeoPrototypeWebApi/GeoPrototypeWebApi/Controllers/NearObjectsController.cs
@@ -0,0 +1,19 @@
+﻿using GeoPrototypeWebApi.Facades;
+using GeoPrototypeWebApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace GeoPrototypeWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class NearObjectsController : Controller
+    {
+        [HttpGet("{latitude}/{longtitude}")]
+        public IEnumerable<InfrastructureObjectsMobileInfo> Get(double latitude, double longitude)
+        {
+            var facade = new MobileDataFacade();
+
+            return facade.GetNearObjects(latitude, longitude);
+        }
+    }
+}
