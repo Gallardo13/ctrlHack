524d005b2d363f8c1c6c394200aa26e80f6ba130
Nikolay Chapkin <nic-ch-work@yandex.ru>
2018-09-22 20:08:34 +0300

    Добавил пустой контроллер для работы с отзывами об объекте

---
+++ GeoPrototypeWebApi/GeoPrototypeWebApi/Controllers/InfrastructureObjectReviewsController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GeoPrototypeWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class InfrastructureObjectReviewsController : Controller
+    {
+    }
+}
