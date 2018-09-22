9b89613ee4e2e78256ab48c98017d4368567fc7e
Gallardo13 <ganibal53@bk.ru>
2018-09-22 16:41:45 +0300

    Commit1

--- GeoPrototypeWebApi/GeoPrototypeWebApi/Controllers/MobileController.cs
+++ GeoPrototypeWebApi/GeoPrototypeWebApi/Controllers/MobileController.cs
@@ -1,35 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace GeoPrototypeWebApi.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+    public class MobileController : Controller
     {
-        // GET api/values
+        // GET: api/values
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        public string Get(int id)
         {
             return "value";
         }
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody] string value)
+        public void Post([FromBody]string value)
         {
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void Put(int id, [FromBody]string value)
         {
         }
 
---
+++ GeoPrototypeWebApi/GeoPrototypeWebApi/Models/InfrastructureObjectMapInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace GeoPrototypeWebApi.Models
+{
+    public class InfrastructureObjectMapInfo
+    {
+
+        public int Id { get; set; }
+
+        public string TempAddress { get; set; }
+
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public string TempObjectType { get; set; }
+
+        public string ObjectType { get; set; }
+
+        public string WorkType { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime FinishDate { get; set; }
+
+        public string CustomerName { get; set; }
+
+        public string CustomerPhone { get; set; }
+
+        public string ContractorName { get; set; }
+
+        public string ContractorPhone { get; set; }
+
+        public string Url { get; set; }
+
+
+
+        public ObjectMapInfo()
+        {
+        }
+    }
+}
