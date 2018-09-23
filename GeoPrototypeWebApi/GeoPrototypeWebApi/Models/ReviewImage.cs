13b252c79400e0a3cdea92e5cf7df393567cb34b
Gallardo13 <ganibal53@bk.ru>
2018-09-23 12:54:59 +0300

    commit

---
+++ GeoPrototypeWebApi/GeoPrototypeWebApi/Models/ReviewImage.cs
@@ -0,0 +1,13 @@
+﻿namespace GeoPrototypeWebApi.Controllers
+{
+    public class ReviewImage
+    {
+        public long Id { get; set; }
+
+        public long ReviewId { get; set; }
+
+        public string MimeType { get; set; }
+
+        public string Image { get; set; }
+    }
+}
